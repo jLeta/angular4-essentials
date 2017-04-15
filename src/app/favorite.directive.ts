475e20f903dc5f6ec1972e622bca95a2f0db1e28
ljifar <leta.jifar@amadeus.com>
2017-04-15 08:48:39 +0200

    add host binding and host listener

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,15 +1,32 @@
-import {Directive, HostBinding, Input} from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
-    selector:'[mwFavorite]'
+    selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
-    //for now lets just set it to true.
-    @HostBinding('class.is-favorite') isFavorite = true;
+    // host binding of css class is-favorite. using the field isFavorite
+    @HostBinding('class.is-favorite') isFavorite;
 
-    @Input() 
-    set mwFavorite(value){
-         this.isFavorite = value;
+    //another binding for mouse havoring. set default value of false 
+    @HostBinding('class.is-favorite-hovering') havoring = false;
+
+    //listen to mouse enter 
+    @HostListener('mouseenter')
+    onMouseEnter() {
+        this.havoring = true;
     }
 
+    //listen to mouse leave 
+    @HostListener('mouseleave')
+    onMouseLeave() {
+        this.havoring = false;
+    }
+
+    /**
+     * set value of isFavorite is set depending on is media
+     */
+    @Input()
+    set mwFavorite(value) {
+        this.isFavorite = value;
+    }
 }
